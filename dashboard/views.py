6130c3f312724573b5bb8efa4c1fa11048ef5ff2
Manav Jagtap <jmanavd@gmail.com>
2026-08-24 21:51:52 +0530

    Added dynamic resource analysis dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,39 @@
+import pandas as pd
 from django.shortcuts import render
 
 
 def home(request):
-    return render(request, "dashboard/home.html")
+    data = pd.read_csv("data/cloud_resources.csv")
+
+    total_current_cost = data["Monthly_Cost"].sum()
+    total_potential_saving = 0
+
+    for index, row in data.iterrows():
+
+        monthly_cost = row["Monthly_Cost"]
+
+        if row["Avg_CPU"] < 5 and row["Avg_RAM"] < 10:
+            estimated_saving = monthly_cost
+
+        elif row["Avg_CPU"] < 20 and row["Avg_RAM"] < 30:
+            estimated_saving = monthly_cost * 0.40
+
+        else:
+            estimated_saving = 0
+
+        total_potential_saving += estimated_saving
+
+    optimized_cost = total_current_cost - total_potential_saving
+
+    saving_percentage = (
+        total_potential_saving / total_current_cost
+    ) * 100
+
+    context = {
+        "total_cost": total_current_cost,
+        "potential_saving": total_potential_saving,
+        "optimized_cost": optimized_cost,
+        "saving_percentage": round(saving_percentage, 2),
+    }
+
+    return render(request, "dashboard/home.html", context)
